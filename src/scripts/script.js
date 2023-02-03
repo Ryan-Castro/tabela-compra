7fd5a896bde86156488470d37942aa7a7ae0798a
Ryan da Silva de Castro Pereira <ryandasilva1008@gmail.com>
2023-02-03 14:44:23 -0300

    ajustes

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -19,8 +19,8 @@ async function init(){
             tProdB.innerHTML += `<tr id="item-${produto}">
                                     <td>${produtos[produto].nome}</td>
                                     <td class="tdInput"><input type="number" onInput="update('${produto}', ${i})" min="0"/></td>
-                                    <td>${produtos[produto].preçoBase.toFixed(2)}</td>
-                                    <td></td>
+                                    <td class="price">${produtos[produto].preçoBase.toFixed(2)}</td>
+                                    <td class="price"></td>
                                 </tr>`
         })
     })
@@ -41,33 +41,62 @@ function calcular(preço){
         let valor = produtos[id][preço] * compras[id]
         let peso = produtos[id]["peso"] * compras[id]
         valorTotal += valor
-        pesoTot += (peso/1000)
+        pesoTot += peso
         $(`#item-${id}`).children[3].innerHTML = valor.toFixed(2) 
     })
     Object.keys(produtos).forEach((produto, i)=>{
         $(`#item-${produto}`).children[2].innerHTML = produtos[produto][preço].toFixed(2) 
     })
     $(`#soma`).innerHTML = `R$: ${valorTotal.toFixed(2)}`
-    $(`#peso`).innerHTML = `Kg: ${pesoTot.toFixed(2)}`
-    if(valorTotal >= 300 && preço == "preçoBase"){
-        $("#phase").innerHTML = "Tabala R$ 300,00"
-        calcular("preço300")
+    $(`#peso`).innerHTML = `Kg: ${pesoTot.toFixed(3)}`
+    if(preço == "preçoBase"){
+        let valueCalc = 0
+        Object.keys(compras).forEach((id)=>{
+            let value = produtos[id]["preço300"] * compras[id]
+            valueCalc += value
+        })
+        if(valueCalc >= 300){
+            document.querySelectorAll(".price").forEach(element=>{
+                element.style.backgroundColor = "orange"
+            })
+            $("#phase").innerHTML = "Tabala R$ 300,00"
+            calcular("preço300")
+        }
     }
-    if(valorTotal >= 600 && preço == "preço300"){
-        $("#phase").innerHTML = "Tabala R$ 600,00"
-        calcular("preço600")
+    if(preço == "preço300"){
+        let valueCalc = 0
+        Object.keys(compras).forEach((id)=>{
+            let value = produtos[id]["preço600"] * compras[id]
+            valueCalc += value
+        })
+        if(valueCalc >= 600){
+            document.querySelectorAll(".price").forEach(element=>{
+                element.style.backgroundColor = "rgb(46, 255, 46)"
+            })
+            $("#phase").innerHTML = "Tabala R$ 600,00"
+            calcular("preço600")
+        }   
     }
-    if(valorTotal >= 1500 && preço == "preço600"){
-        $("#phase").innerHTML = "Tabala R$ 1500,00"
-        calcular("preço1500")
+    if(preço == "preço600"){
+        let valueCalc = 0
+        Object.keys(compras).forEach((id)=>{
+            let value = produtos[id]["preço1500"] * compras[id]
+            valueCalc += value
+        })
+        if(valueCalc >= 1500){
+            document.querySelectorAll(".price").forEach(element=>{
+                element.style.backgroundColor = "cyan"
+            })
+            $("#phase").innerHTML = "Tabala R$ 1500,00"
+            calcular("preço1500")
+        }
     }
 }
 
 function check(){
     let cadastro = $("input[name='register']:checked").value
     let name = $("#name").value
-    let address = $("#address").value
-    if(name == "" || address == ""){
+    if(name == ""){
         alert("preencha seus dados")
         return
     } 
@@ -79,9 +108,7 @@ function check(){
         $("#modal").style.display = "flex"
         return
     }
-
     enviar()
-
 }
 
 function createCadastro(){
@@ -92,7 +119,9 @@ function createCadastro(){
     let estado  = $("#estado").value
     let tel = $("#tel").value
     let email = $("#email").value
-    formulario = `*Numero*:%20${numero}%0A*Bairro*:%20${bairro}%0A*Cep*:%20${cep}%0A*Cidade*:%20${cidade}%0A*Estado*:%20${estado}%0A*Tel*:%20${tel}%0A*Email*:%20${email}%0A`
+    let address = $("#address").value
+    let cpf = $("#cpf").value
+    formulario = `*Numero*:%20${numero}%0A*Bairro*:%20${bairro}%0A*Cep*:%20${cep}%0A*Cidade*:%20${cidade}%0A*Estado*:%20${estado}%0A*Tel*:%20${tel}%0A*Email*:%20${email}%0A*Endereço*:%20${address}%0A*CPF/CNPJ*:%20${cpf}%0A`
     enviar()
 }
 
@@ -102,11 +131,12 @@ function enviar(){
     let name = $("#name").value
     let address = $("#address").value
     let pedido = ""
+    let payment = $("#payment").options[$("#payment").selectedIndex].value
     Object.keys(compras).forEach((id)=>{
         pedido += `*${produtos[id].nome}*: ${compras[id]} %0A`
     })
     location.href = `
-                    https://wa.me/5551998116453?text=*Novo%20Pedido*%0A------------------------------%0A%0A*Nome*:%20${name.replaceAll(" ", "%20")}%0A*Endere%C3%A7o*:%20${address.replaceAll(" ", "%20")}%0A*J%C3%A1%20%C3%A9%20cadastrado*:%20${cadastro}%0A${formulario}%0A------------------------------%0AProdutos:%0A%0A${pedido.replaceAll(" ", "%20")}%0A------------------------------%0A%0A*Valor%20Total%20Sem%20o%20Frete*:%20R$${valorTotal.toString().replace(".", ",")}%0A*Peso%20Total*:%20${pesoTot.toString().replace(".", ",")}Kg%0AForma%20de%20Pagamento:%20PIX%0A
+                    https://wa.me/5551998116453?text=*Novo%20Pedido*%0A------------------------------%0A%0A*Nome*:%20${name.replaceAll(" ", "%20")}%0A*Endere%C3%A7o*:%20${address.replaceAll(" ", "%20")}%0A*J%C3%A1%20%C3%A9%20cadastrado*:%20${cadastro}%0A${formulario}%0A------------------------------%0AProdutos:%0A%0A${pedido.replaceAll(" ", "%20")}%0A------------------------------%0A%0A*Valor%20Total%20Sem%20o%20Frete*:%20R$${valorTotal.toString().replace(".", ",")}%0A*Peso%20Total*:%20${pesoTot.toString().replace(".", ",")}Kg%0AForma%20de%20Pagamento:%20${payment}%0A
                         `
 }
 
